b3f174ed3e935e42256b8d158b79e3d783cada5b
Jesus Paz <jesuspaz145@gmail.com>
2026-01-21 11:53:38 -0600

    port

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     # Run the application using Uvicorn
     # allowing execution with "python run.py"
     print("Iniciando servidor en http://localhost:5000...")
-    uvicorn.run("app.main:app", host="0.0.0.0", port=4321, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=5000, reload=True)
